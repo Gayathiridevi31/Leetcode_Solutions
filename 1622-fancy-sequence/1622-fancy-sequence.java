19959d886d21b5c9cb52030d9e590543d897e6cf
Gayathiridevi31 <gayathiridevik2006@gmail.com>
2026-03-15 12:21:05 +0530

    Time: 45 ms (93.1%), Space: 124.5 MB (58.62%) - LeetHub

---
+++ 1622-fancy-sequence/1622-fancy-sequence.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Fancy {
+
+    long mod = 1000000007;
+    List<Long> list;
+    long mul = 1;
+    long add = 0;
+
+    public Fancy() {
+        list = new ArrayList<>();
+    }
+
+    public void append(int val) {
+        long normalized = ((val - add + mod) % mod) * modInverse(mul) % mod;
+        list.add(normalized);
+    }
+
+    public void addAll(int inc) {
+        add = (add + inc) % mod;
+    }
+
+    public void multAll(int m) {
+        mul = (mul * m) % mod;
+        add = (add * m) % mod;
+    }
+
+    public int getIndex(int idx) {
+        if (idx >= list.size()) return -1;
+
+        long val = list.get(idx);
+        long result = (val * mul % mod + add) % mod;
+
+        return (int) result;
+    }
+
+    private long modInverse(long x) {
+        return power(x, mod - 2);
+    }
+
+    private long power(long a, long b) {
+        long res = 1;
+
+        while (b > 0) {
+            if ((b & 1) == 1)
+                res = (res * a) % mod;
+
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+
+        return res;
+    }
+}
