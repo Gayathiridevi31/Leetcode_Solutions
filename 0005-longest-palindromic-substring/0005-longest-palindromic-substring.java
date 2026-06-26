31fb6da74405fa7d20d16222d095a3c54da5b3a7
Gayathiridevi31 <gayathiridevik2006@gmail.com>
2026-06-26 12:10:38 +0530

    Time: 20 ms (36.71%), Space: 47 MB (17.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,29 +1,27 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-        
-        int start = 0, end = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            int l1 = func(s, i, i);      // odd length
-            int l2 = func(s, i, i + 1);  // even length
-            int len = Math.max(l1, l2);
-            
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
-        }
-        
-        return s.substring(start, end + 1);
-    }
-    
-    public int func(String s, int left, int right) {
-        while (left >= 0 && right < s.length() 
-                && s.charAt(left) == s.charAt(right)) {
+    static String expand(String str,int left,int right)
+    {
+        while(left>=0 && right<str.length() && str.charAt(left)==str.charAt(right))
+        {
             left--;
             right++;
         }
-        return right - left - 1;  // correct length
+        return str.substring(left+1,right);
+    }
+    public String longestPalindrome(String s) {
+        String longer="";
+        for(int i=0;i<s.length();i++)
+        {
+            String odd=expand(s,i,i);
+            String even=expand(s,i,i+1);
+            String result=odd.length()>even.length() ? odd :even;
+            if(result.length()>longer.length())
+            {
+                longer=result;
+            }
+        }
+        return longer;
+
+        
     }
 }
